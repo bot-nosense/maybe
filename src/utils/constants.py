287f1ccfc8ded7e5e51476608d5f5c4afc380f52
Vu Doan <uchihanosense@gmail.com>
2023-06-05 10:17:17 +0700

    utils

---
+++ src/utils/constants.py
@@ -0,0 +1,22 @@
+
+NATURE_TYPE = {
+    'xuôi': 'Yes.',
+    'ngược': 'No.',
+}
+
+ONLY_MINOR_ARCANA_TYPE = {
+    'Wands': 'Yes.',
+    'Cups': 'No.',
+    'Chalices': 'No.',
+    'Pentacles': 'No.',
+    'Swords': 'Yes.',
+}
+
+MEANING_TYPE = {
+    'Wands': 'Yes, nhưng bạn phải nỗ lực để có được điều đó.',
+    'Cups': 'Yes, chắc chắn bạn sẽ có được điều bạn muốn mà không càn phải làm gì cả.',
+    'Chalices': 'Yes, chắc chắn bạn sẽ có được điều bạn muốn mà không càn phải làm gì cả.',
+    'Pentacles': 'Yes, bạn sẽ phải có một sự đánh đổi, trả giá để có được nó.',
+    'Swords': 'No.',
+}
+
