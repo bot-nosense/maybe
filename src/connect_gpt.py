ad8fe7f7cdbdca71ea2db1265a1d9250b6ac84cd
Vu Doan <uchihanosense@gmail.com>
2023-06-07 08:09:10 +0700

    connect chat_gpt

--- src/connect_gpt.py
+++ src/connect_gpt.py
@@ -1,31 +1,31 @@
-import requests
+# import requests
 
-def chat_with_gpt(message):
-    api_key = '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'
-    url = 'https://api.openai.com/v1/chat/completions'
+# def chat_with_gpt(message):
+#     api_key = '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'
+#     url = 'https://api.openai.com/v1/chat/completions'
     
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {api_key}'
-    }
+#     headers = {
+#         'Content-Type': 'application/json',
+#         'Authorization': f'Bearer {api_key}'
+#     }
     
-    data = {
-        'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
-                    {'role': 'user', 'content': message}]
-    }
+#     data = {
+#         'messages': [{'role': 'system', 'content': 'You are a helpful assistant.'},
+#                     {'role': 'user', 'content': message}]
+#     }
     
-    response = requests.post(url, json=data, headers=headers)
-    response_data = response.json()
+#     response = requests.post(url, json=data, headers=headers)
+#     response_data = response.json()
     
-    if 'choices' in response_data:
-        choices = response_data['choices']
-        if choices and 'message' in choices[0]:
-            return choices[0]['message']['content']
+#     if 'choices' in response_data:
+#         choices = response_data['choices']
+#         if choices and 'message' in choices[0]:
+#             return choices[0]['message']['content']
     
-    return "Ê mầy, tôi không có hiểu."
+#     return "Ê mầy, tôi không có hiểu."
 
-def call_gpt():
-    # Gửi tin nhắn cho ChatGPT và nhận phản hồi
-    user_message = input("Nhập tin nhắn của bạn: ")
-    response = chat_with_gpt(user_message)
-    print(response)
+# def call_gpt():
+#     # Gửi tin nhắn cho ChatGPT và nhận phản hồi
+#     user_message = input("Nhập tin nhắn của bạn: ")
+#     response = chat_with_gpt(user_message)
+#     print(response)
