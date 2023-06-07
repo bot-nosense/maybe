e7b51d96f1081cc1342979d2ffda96a4acbaede7
Vu Doan <uchihanosense@gmail.com>
2023-06-07 08:09:59 +0700

    functions

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -7,7 +7,8 @@ def yes_no_question():
 
     tarot = Tarots(nb_of_card= 1)
     name_card = tarot.get_info_card()
-    card_type = name_card.split(' - ')[1]
+    card_type = name_card.split(' - ')[0]
+    sub_name = card_type.split(' ')[-1]
     options = random.randint(1, 3)
     print('Card:', name_card)
 
@@ -18,13 +19,12 @@ def yes_no_question():
     match options:
         case 1: 
             print( 'Type: MEANING - Theo tính chất của lá bài' )
-            print( 'Result: ', render_questions(card_list= MEANING_TYPE, card_type= card_type) )
+            print( 'Result: ', render_questions(card_list= MEANING_TYPE, card_type= sub_name) )
         case 2: 
+            card_type = name_card.split(' - ')[1]
             print( 'Type: NATURE - Chỉ có Yes hoặc No' )
             print( 'Result: ', render_questions(card_list= NATURE_TYPE, card_type= card_type) )
         case 3: 
-            card_type = name_card.split(' - ')[0]
-            sub_name = card_type.split(' ')[-1]
             print( 'Type: ONLY_MINOR_ARCANA - Chỉ có ẩn phụ' )
             print( 'Result: ', render_questions(card_list= ONLY_MINOR_ARCANA_TYPE, card_type= sub_name) )
 
@@ -42,11 +42,14 @@ def draw_tarot_cards():
     number_of_card= get_number_of_card()
     tarot = Tarots(nb_of_card= number_of_card)
     card_list = []
+    temp_list = [] # tạo 1 list giả để kiểm tra 2 thẻ giống nhau trong cùng 1 lá bài
 
     for nb in range(number_of_card):
         card = tarot.get_info_card()
-        if (card in card_list) == False: 
+        temp_card = card.split(' - ')[0]
+        if (temp_card in temp_list) == False: 
             card_list.append( card )
+            temp_list.append( temp_card )
 
     for card in card_list:
         print('Card: ', card)
