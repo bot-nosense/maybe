e1c41a5c12624833673b70df5253be747ad46383
Vu Doan <uchihanosense@gmail.com>
2023-06-10 10:16:10 +0700

    main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
-from src.utils.functions import draw_tarot_cards, yes_no_question
+from src.utils.functions.reading_the_deck import draw_tarot_cards, yes_no_question
+
 
 def main():
 
     while True:
         try:
-            choose = int(input("Chọn thể loại 1-Yes/No  2-Rút lá: "))
+            print()
+            choose = int(input("Chọn: 1-Yes/No  2-Rút lá: "))
             if choose > 0: 
                 question = input("Câu hỏi: ")
-                if choose == 1: return yes_no_question()
-                elif choose == 2: return draw_tarot_cards()
+                if choose == 1: yes_no_question(question)
+                elif choose == 2: draw_tarot_cards()
             else: return "Số lá phải lớn hơn 0. Vui lòng nhập lại."
         except ValueError: return "Đầu vào không hợp lệ. Vui lòng nhập lại."
+
+
+
+    
+
     
     
     
