b6911b3cbf034ff0c66eb7367c9b8af2cc90f044
VuDoan <vudv@navisoft.com.vn>
2023-11-01 22:06:40 +0700

    beta verison 0.0.0.3, 20231031 update yes/no questions

--- src/main.py
+++ src/main.py
@@ -2,17 +2,19 @@ from src.object_class.process import Process
 
 
 def main():
+    print('beta verison 0.0.0.3, 20231031 update yes/no questions')
 
     while True:
         try:
             print()
-            choose = int(input("Chọn: 1-Yes/No  2-Rút lá: "))
-            if choose > 0: 
-                question = input("Câu hỏi: ")
-                pr = Process(question)
-                if choose == 1: pr.yes_no_question(question)
-                elif choose == 2: pr.draw_tarot_cards()
-            else: return "Lớn hơn 0, nhập lại đê."
+            # choose = int(input("Chọn: 1-Yes/No  2-Rút lá: "))
+            # if choose > 0: 
+            # if choose ==  1: 
+            question = input("Câu hỏi: ")
+            pr = Process(question)
+            if choose == 1: pr.yes_no_question(question)
+            elif choose == 2: pr.draw_tarot_cards()
+            # else: return "Lớn hơn 0, nhập lại đê."
         except ValueError: return "Nhập lại đê." 
 
 
