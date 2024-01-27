77399833ecb91b0d2be9f825839c4decc83f8618
VuDoan <vudv@navisoft.com.vn>
2024-01-28 06:29:24 +0700

    daily update

--- bot/algorithm_core/yes_no_questions.py
+++ bot/algorithm_core/yes_no_questions.py
@@ -1,13 +1,2 @@
 # Binary algorithm 
 
-class YesNoQuestions:
-
-    def __init__ (self):
-        pass
-
-    def face_up():
-        pass
-
-    def question_results():
-        # các cách yes no được mapping với các số, random ra số nào thì đọc bài bằng cách đó
-        pass
