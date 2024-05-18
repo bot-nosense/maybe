5675bb2f7cecd2c28bd38ca12ad4f379c6daa11a
[BOT] Nosense <uchihanosense@gmail.com>
2024-05-19 06:26:40 +0700

    add new commands

--- bot/data_core/commands.py
+++ bot/data_core/commands.py
@@ -1,4 +1,26 @@
 # commands request to bot
 
 
-MAIN_COMMANDS = ['1 lá', '3 lá', '6 lá', '9 lá', 'một lá', 'ba lá', 'sáu lá', 'chín lá', '7 lá', 'bảy lá', '12 lá', '5 lá', 'năm lá']
+MAIN_COMMANDS = [
+    '1 lá', 
+    '3 lá', 
+    '6 lá', 
+    '9 lá', 
+    '7 lá', 
+    '12 lá', 
+    '5 lá'
+]
+
+START_COMMANDS = {
+    '-1': 'the pisma vision tarot'
+}
+
+TAKE_CARD_COMMAND = {
+    '-1': '1',
+    '-3': '3',
+    '-5': '5',
+    '-6': '6',
+    '-7': '7',
+    '-9': '9',
+    '-12': '12',
+}
